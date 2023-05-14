1ec39dfb476681c068b82989dee95fb0a97f8289
Claudio Araujo <calloman123@gmail.com>
2023-05-13 22:44:38 -0600

    Init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.markdown(
+    """
+# Diccionario Métodos Numéricos II
+**Proyecto final**
+
+---
+
+## Integrantes:
+- Araujo Palestina Claudio Hassiel
+- Hernández Rodríguez Alejandro
+
+## Temas:
+- Newton
+- Casi-Newton
+"""
+)
+
+# Texto que aparece en la barra lateral
+st.sidebar.markdown(
+    """
+# Diccionario
+
+**Proyecto final de metodos numericos 2**
+- Araujo Palestina Claudio Hassiel
+- Hernández Rodríguez Alejandro
+"""
+)
